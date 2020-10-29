449f538f822ab4fd260b8734535a30dcfc9b4770
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-10-29 13:52:12 +0700

    nothing

--- js/user.js
+++ js/user.js
@@ -1,8 +1,25 @@
 class User {
-    constructor(name, age, userInfo) {
+    constructor(name, age, images, desc) {
         this._name = name;
         this._age = age;
-        this._userInfo = userInfo;
+        this._images = images;
+        this._desc = desc;
+    }
+
+    get images() {
+        return this._images;
+    }
+
+    set images(value) {
+        this._images = value;
+    }
+
+    get desc() {
+        return this._desc;
+    }
+
+    set desc(value) {
+        this._desc = value;
     }
 
     get name() {
@@ -22,18 +39,10 @@ class User {
     }
 
 
-    get userInfo() {
-        return this._userInfo;
-    }
-
-    set userInfo(value) {
-        this._userInfo = value;
-    }
-
     getInformation() {
         let str = "<ul>" +
             "<li>" +
-            "<h3>" +`${this.name},${this.age}`+ "</h3></li>" +
+            "<h3>" + `${this.name},${this.age}` + "</h3></li>" +
             "<li></li>" + "<li></li>" + "<li></li>" + "<li></li>" + "<li></li>" +
             "</ul>"
     }
