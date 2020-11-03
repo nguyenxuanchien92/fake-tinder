61103c6045fed9b8aefcbe7f86bf40a0a60ac669
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-03 10:44:47 +0700

    đã hoàn thành việc phân biệt phím button

--- js/user-manage.js
+++ js/user-manage.js
@@ -7,7 +7,7 @@ desc.height = "162 cm";
 desc.career = "beU Model Management";
 desc.degree = "Trường đại học Quốc gia Hà Nội";
 
-let images = ["../tinder-fake/images/hot_girl_03.jpg","../tinder-fake/images/hot_girl_02.jpg","../tinder-fake/images/hot_girl_01.jpg"];
+let images = ["../tinder-fake/images/hot_girl_03.jpg", "../tinder-fake/images/hot_girl_02.jpg", "../tinder-fake/images/hot_girl_01.jpg"];
 // let src = '../tinder-fake/images/';
 
 
@@ -15,26 +15,22 @@ let user = new User("Thu Hà", 27, images, desc);
 
 
 //chưa kiểm soát được việc kiểm tra giá trị -buttonId-
-function selectButton(id){
-    let buttons = document.getElementsByTagName('button');
-
-    for (let i = 0; i < buttons.length; i++) {
-        let buttonId = buttons[i].getAttribute('id');
-        switch (buttonId){
-            case 'btn-super-like':
-                alert(`btn-super-like ${id}`);
-                return;
-            case 'btn-like':
-                alert(`btn-like ${id}`);
-                return;
-            case 'btn-dis-like':
-                alert(`btn-dis-like ${id}`);
-                return;
-        }
+function selectButton(id) {
+    switch (id) {
+        case 'btn-super-like':
+            alert(`btn-super-like ${id}`);
+            break;
+        case 'btn-like':
+            alert(`btn-like ${id}`);
+            break;
+        case 'btn-dis-like':
+            alert(`btn-dis-like ${id}`);
+            break;
     }
+
 }
 
-function show(){
+function show() {
     content.innerHTML = user.getInformation();
 }
 
